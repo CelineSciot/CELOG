3a36c07adaca279976a71982da0c4f1a116d30a4
CelineSciot <celine.sciot@hotmail.com>
2017-11-13 12:09:05 +0100

    llll

--- CELOG/BaseDonnees/Tables.sql
+++ CELOG/BaseDonnees/Tables.sql
@@ -10,3 +10,10 @@ quantite_Prod int not null,
 categorie_Prod varchar(20),
 promo_Prod double,
 primary key(id_Prod))ENGINE= INNODB;
+
+
+insert into Produit(prix_Prod,label_Prod,quantite_Prod,categorie_Prod,promo_Prod ) VALUES
+(1.20,"Banane",10,"FRUIT",0),
+(1.03,"Pomme",15,"FRUIT",0),
+(0.94,"Tomate",20,"FRUIT",0),
+(0.80,"Noix",30,"FRUIT",0.125);
