4b8a4ba2db828b29c561818c9bdfe6a0ca874897
logan <logan.carlier@gmail.com>
2017-11-16 14:21:35 +0100

    J'ai fais pour que toutes les requêtes fonctionne la seule que je n'y arrive pas c'est la Get(int id)

--- CELOG/Models/ProduitDAO.cs
+++ CELOG/Models/ProduitDAO.cs
@@ -10,10 +10,10 @@ namespace CELOG.Models
     public static class ProduitDAO
     {
         private static readonly string QUERY = "SELECT * FROM Produit";
-        private static readonly string GET = QUERY + "WHERE id_prod = @id";
+        private static readonly string GET = QUERY + "WHERE id_Prod = @id_Prod";
         private static readonly string CREATE = "INSERT INTO Produit(qte_Prod,label_Prod,prix_Prod,promo_Prod,categorie_Prod,urlImage_Prod) OUTPUT INSERTED.id_Prod VALUES(@qte_Prod,@label_Prod,@prix_Prod,@promo_Prod,@categorie_Prod,@UrlImage_Prod) ";
         private static readonly string DELETE = "DELETE FROM produit WHERE id_prod = @id";
-        private static readonly string UPDATE = "UPDATE Produit SET qte_Prod=@qte_Prod ,label_Prod= @label_Prod,prix_Prod=@prix_Prod ,promo_Prod=@promo_Prod ,categorie_Prod=@categorie_Prod, urlImage_Prod=@URlImage_Prod";
+        private static readonly string UPDATE = "UPDATE Produit SET qte_Prod=@qte_Prod ,label_Prod= @label_Prod,prix_Prod=@prix_Prod ,promo_Prod=@promo_Prod ,categorie_Prod=@categorie_Prod, urlImage_Prod=@urlImage_Prod WHERE id_Prod = @id_Prod";
 
         public static List<Produit> GetAllProduit()
         {
@@ -26,8 +26,7 @@ namespace CELOG.Models
                 SqlCommand command = new SqlCommand(QUERY, connection);
 
                 SqlDataReader reader = command.ExecuteReader();
-                
-                //TODO
+               
                 while (reader.Read())
                 {
                     Produit prod = new Produit(
@@ -46,28 +45,29 @@ namespace CELOG.Models
         
         public static Produit Get(int id)
         {
-            Produit produit = null;
-
+            Produit produit = new Produit();
 
             using (SqlConnection conn = DataBase.GetSqlConnection())
             {
                 conn.Open();
-
+               
                 SqlCommand command = new SqlCommand(GET, conn);
                 command.Parameters.AddWithValue("@id_Prod", id);
+                // EXECPTION ICI avec le =
+                //System.Data.SqlClient.SqlException : 'Incorrect syntax near '='.'
 
                 SqlDataReader reader = command.ExecuteReader();
-                //TODO
+               
                 if (reader.Read())
                 {
                     produit = new Produit(
-                        reader.GetInt32(0),
-                        reader.GetInt32(1),
-                        reader.GetString(2),
-                        reader.GetDecimal(3),
-                        reader.GetDecimal(4),
-                        reader.GetString(5),
-                        reader.GetString(6)
+                       reader.GetInt32(0),
+                       reader.GetInt32(1),
+                       reader.GetString(2),
+                       reader.GetDecimal(3),
+                       reader.GetDecimal(4),
+                       reader.GetString(5),
+                       reader.GetString(6)
                                        );
                 }
             }
@@ -79,7 +79,7 @@ namespace CELOG.Models
             using (SqlConnection conn = DataBase.GetSqlConnection())
             {
                 conn.Open();
-                //TODO
+                
                 SqlCommand command = new SqlCommand(CREATE, conn);
                 command.Parameters.AddWithValue("@qte_Prod", produit.Qte_Prod);
                 command.Parameters.AddWithValue("@label_Prod", produit.Label_Prod);
@@ -109,7 +109,7 @@ namespace CELOG.Models
 
             return estSupprime;
         }
-        //TODO
+        
         public static bool Update(Produit produit)
         {
             bool aEteModifie = false;
@@ -118,14 +118,16 @@ namespace CELOG.Models
                 conn.Open();
 
                 SqlCommand command = new SqlCommand(UPDATE, conn);
-                command.Parameters.AddWithValue("@label_Produit", produit.Label_Prod);
+                command.Parameters.AddWithValue("@label_Prod", produit.Label_Prod);
                 command.Parameters.AddWithValue("@qte_Prod", produit.Qte_Prod);
                 command.Parameters.AddWithValue("@prix_Prod", produit.Prix_Prod);
                 command.Parameters.AddWithValue("@categorie_Prod", produit.Categorie_Prod);
                 command.Parameters.AddWithValue("@promo_Prod", produit.Promo_Prod);
-                command.Parameters.AddWithValue("@UrlImage_Prod", produit.UrlImage_Prod);
+                command.Parameters.AddWithValue("@urlImage_Prod", produit.UrlImage_Prod);
+                command.Parameters.AddWithValue("@id_Prod", produit.Id_Prod);
 
-                command.Parameters.AddWithValue("@id", produit.Id_Prod);
+
+                aEteModifie = command.ExecuteNonQuery() != 0;
 
             }
             return aEteModifie;
